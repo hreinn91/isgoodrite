39fe85ae74692baa9ba1357239016e5d4d76d93e
hreinn91 <hreinnjuliusson@gmail.com>
2025-02-24 21:56:33 +0100

    Add requirements and write to copy file

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-###
+# IS GOOD RITE?!!
+
+`isgoodrite` is a simple command line tool which integrates with a local running ollama.
+Use your local ollama to validate your scripts and code using `isgoodrite`.
 
 
+## SETUP
+
+Setup this tool by installing it with:
+
+    pip install -r requirements.txt
+    pip install -e .
 
 
+## HOW IT WORKS
+
+`isgoodrite` takes a scripts as an input and writes an improved script as a copy of the input file.
 
 ## INTEGRATE WITH DIFF-SO-FANCY
 
@@ -10,5 +22,5 @@ https://github.com/so-fancy/diff-so-fancy
 
 `diff-so-fancy`is a great tool for making diffs human-readable. 
 
-Integrate isgoodrite with diff-so-fancy, all you need is the diff-so-fancy binary  
+Integrate `isgoodrite` with diff-so-fancy, all you need is the `diff-so-fancy` binary in your path and the good old `diff`.  
 
--- isgoodrite/__main__.py
+++ isgoodrite/__main__.py
@@ -1,4 +1,6 @@
 import sys
+import subprocess
+import os
 
 from isgoodrite.query_model import query_llm
 from isgoodrite.process_answer import ModelResponse
@@ -12,17 +14,36 @@ def read_file(file_name):
         print(f"No such file: {file_name}")
     pass
 
-def write_file(original_file_name):
-    pass
+def get_copy_name(original_file_name):
+    if '.' not in original_file_name:
+        return original_file_name + "-copy"
+    file_name_split = original_file_name.split('.')[:-1]
+    name = file_name_split[-1]
+    copy_name = file_name_split[-1] + "-copy"
+    if name == "" or '.' not  in original_file_name:
+        return original_file_name + "-copy"
+    return original_file_name.replace(name, copy_name)
+
+def write_file(original_file_name, file_content):
+    copy_file_name = get_copy_name(original_file_name)
+    print(f"Writing suggestion to: {copy_file_name}")
+    with open(copy_file_name, 'w+') as f:
+        f.write(file_content)
+    return copy_file_name
 
 def main():
-    args = sys.argv[1:]
-    model_responses = []
-    for file_name in args:
-        file_content = read_file(file_name)
-        answer = query_llm(file_name, file_content)
-        model_responses.append(ModelResponse(answer))
+    file_name = sys.argv[1]
+    file_content = read_file(file_name)
+    answer = query_llm(file_name, file_content)
+    model_response = ModelResponse(answer)
+    copy_file_name = write_file(file_name, model_response.code)
 
+    subprocess.call(f"diff -u {file_name} {copy_file_name} | diff-so-fancy", shell=True)
+    try:
+        os.remove(copy_file_name)
+    except OSError:
+        print(f"Failed to clean up copy file {copy_file_name}")
+        pass
 
 
 if __name__ == '__main__':
